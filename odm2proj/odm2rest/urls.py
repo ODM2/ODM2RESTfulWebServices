44f755f010fa4a08e3b046186488d76f39552cc5
Ubuntu <azureuser@sis-devel.sis-devel.d4.internal.cloudapp.net>
2015-06-01 23:42:54 +0000

    add new endpoints

--- odm2proj/odm2rest/urls.py
+++ odm2proj/odm2rest/urls.py
@@ -9,16 +9,17 @@ from odm2rest.site_views import SiteViewSet, SiteSamplingFeatureCodeViewSet, Sit
 from odm2rest.action_views import ActionsViewSet, ActionIDViewSet
 from odm2rest.value_views import ValuesViewSet
 from odm2rest.result_views import ResultsViewSet, ResultsVarCodeViewSet, ResultsSFCodeViewSet, ResultsSFUUIDViewSet, ResultsBBoxViewSet, ResultsActionDateViewSet, ResultsRTypeCVViewSet, ResultsComplexViewSet
-from odm2rest.method_views import MethodCodeViewSet
-from odm2rest.org_views import OrgCodeViewSet
+from odm2rest.method_views import MethodsViewSet, MethodCodeViewSet
+from odm2rest.org_views import OrgsViewSet, OrgCodeViewSet
 from odm2rest.dataset_views import DatasetViewSet
 from odm2rest.datasetdetail_views import DatasetDetailViewSet
 
 urlpatterns = [
     url(r'^$', views.api_root),
+    url(r'^methods/$', MethodsViewSet.as_view(), name='method-list'),
     url(r'^methods/(?P<methodCode>.+)/$', MethodCodeViewSet.as_view(), name='method-detail'),
+    url(r'^organizations/$', OrgsViewSet.as_view(), name='org-list'),
     url(r'^organizations/(?P<organizationCode>.+)/$', OrgCodeViewSet.as_view(), name='org-detail'),
-    url(r'^sites/$',  SiteViewSet.as_view(), name='site-list'),
     url(r'^actions/$', ActionsViewSet.as_view(), name='action-list'),
     url(r'^actions/(?P<actionID>.+)/$', ActionIDViewSet.as_view(), name='action-list'),
     url(r'^Observations/$', ResultsViewSet.as_view(), name='observ-list'),
@@ -33,6 +34,7 @@ urlpatterns = [
     url(r'^Observations/value/(?P<resultUUID>.+)/$', ValuesViewSet.as_view(), name='value-list'),
     url(r'^variables/$', VariableViewSet.as_view(), name='variable-list'),
     url(r'^variables/(?P<variableCode>.+)/$', VariableCodeViewSet.as_view(), name='variable-detail'),
+    url(r'^sites/$',  SiteViewSet.as_view(), name='site-list'),
     url(r'^sites/samplingfeatureCode/(?P<samplingfeatureCode>.+)/$', SiteSamplingFeatureCodeViewSet.as_view(), name='site-detail-sfc'),
     url(r'^sites/siteType/(?P<siteType>.+)/$',  SiteTypeViewSet.as_view(), name='site-detail-st'),
     url(r'^datasets/$', DatasetViewSet.as_view(), name='dataset-list'),
