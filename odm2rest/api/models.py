f1c9b08a3c064b2a5a314dc24c73c7ffcdb4f1b0
lsetiawan <landungs@uw.edu>
2017-09-19 13:35:14 -0700

    Add Affiliations, Person, and Organization models

--- odm2rest/api/models.py
+++ odm2rest/api/models.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from __future__ import unicode_literals
+from __future__ import division
 
-from django.db import models
 
 # Create your models here.
+class Affiliations(object):
+
+    def __init__(self, person, organization, poc, startdate, enddate, phone, email, address, web_link):
+        self.Person = person
+        self.Organization = organization
+        self.IsPrimaryOrganizationContact = poc
+        self.AffiliationStartDate = startdate
+        self.AffiliationEndDate = enddate
+        self.PrimaryPhone = phone
+        self.PrimaryEmail = email
+        self.PrimaryAddress = address
+        self.PersonLink = web_link
+
+class Person(object):
+
+    def __init__(self, firstname, middlename, lastname):
+        self.PersonFirstName = firstname
+        self.PersonMiddleName = middlename
+        self.PersonLastName = lastname
+
+class Organization(object):
+
+    def __init__(self, type, code, name, description, web_link, parent_org):
+        self.OrganizationTypeCV = type
+        self.OrganizationCode = code
+        self.OrganizationName = name
+        self.OrganizationDescription = description
+        self.OrganizationLink = web_link
+
+        self.ParentOrganizationID = parent_org
