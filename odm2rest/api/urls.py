5a5deb39ded61082a2eafc9794f51295e93e58ac
lsetiawan <landungs@uw.edu>
2017-09-19 17:10:28 -0700

    Use most basic swagger

--- odm2rest/api/urls.py
+++ odm2rest/api/urls.py
@@ -6,11 +6,19 @@ from __future__ import division
 
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from views import AffiliationsViewSet, PeopleViewSet
+from rest_framework_swagger.views import get_swagger_view
+
+from views import (
+    AffiliationsViewSet,
+    PeopleViewSet
+)
+
+schema_view = get_swagger_view(title='ODM2 REST API')
 
 urlpatterns = [
+    url(r'^docs/$', schema_view),
     url(r'^affiliations$', AffiliationsViewSet.as_view(), name='affiliations-list'),
-    url(r'^people', PeopleViewSet.as_view(), name='people-list')
+    url(r'^people$', PeopleViewSet.as_view(), name='people-list')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html', 'yaml', 'csv'])
