b648ce107439d5c113c867178c4b5c19248b5950
lsetiawan <landungs@uw.edu>
2017-10-02 10:19:15 -0700

    Covert swagger.yaml to swagger.json as soon as server spins up

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "odm2rest.settings")
+    from api.utils import swagger_convert
+    swagger_convert()
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
