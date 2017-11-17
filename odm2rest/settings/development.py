f609f5a82815dca7f46f6192658fe56d03d67190
Elijahwalkerwest <elijahwalkerwest@gmail.com>
2017-11-17 14:38:20 -0700

    Update development.py
    Removed Db info, added generic DB info back .

--- odm2rest/settings/development.py
+++ odm2rest/settings/development.py
@@ -17,10 +17,10 @@ SWAGGER_SETTINGS['BASE_DOMAIN'] = '127.0.0.1:8000'
 
 # SQLAlchemy settings
 ODM2DATABASE = {
-    'engine': 'postgresql',
-    'address': 'odm2wofpy1.uwrl.usu.edu',
+    'engine': 'db engine',
+    'address': 'localhost',
     'port': 5432,
-    'db': 'odm2',
-    'user': 'dbadmin',
-    'password': 'pinkbananastastegross'
+    'db': 'db name',
+    'user': 'username',
+    'password': 'mypassword'
 }
