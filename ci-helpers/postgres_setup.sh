43d8046ed5bfdd47ef0235cbe7bd1151bca1e43f
lsetiawan <landungs@uw.edu>
2017-11-28 23:25:38 -0800

    Use db from jeff

--- ci-helpers/postgres_setup.sh
+++ ci-helpers/postgres_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 psql -U postgres -c "create extension postgis"
-psql -U postgres -q  -f ./tests/data/envirodiy_postgres_odm2.sql
+psql -U postgres -q  -f ./tests/data/odm2samples_dump.sql
